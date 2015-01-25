f3f70b71ec5db98bedac33d66022b0b93908ead1
azhi <astzhe@gmail.com>
2015-01-25 19:25:50 +0300

    get rid of vim config

--- dotvim/find-ruby.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ '/opt/local/bin/vim' = `which vim` -a -e /opt/local/bin/ruby ]; then
-  echo /opt/local/bin/ruby
-elif [ -e '/usr/bin/ruby' ]; then
-  echo /usr/bin/ruby
-else
-  which ruby
-fi
